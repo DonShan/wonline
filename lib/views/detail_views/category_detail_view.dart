504fd36bb569361eb2bb1a2f0d78a2332cf5adec
DonShan <msdjshan47@gmail.com>
2022-10-11 08:27:58 +0530

    Implemented newTabaleView with json data

---
+++ lib/views/detail_views/category_detail_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryDetailView extends StatefulWidget {
+  const CategoryDetailView({super.key});
+
+  @override
+  State<CategoryDetailView> createState() => _CategoryDetailViewState();
+}
+
+class _CategoryDetailViewState extends State<CategoryDetailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/views/pages/news_page.dart
+++ lib/views/pages/news_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wonline/data/status.dart';
+import 'package:wonline/view_model/category_view_model.dart';
+import 'package:wonline/views/detail_views/category_detail_view.dart';
 
 class NewsPage extends StatefulWidget {
   const NewsPage({super.key});
@@ -8,17 +12,64 @@ class NewsPage extends StatefulWidget {
 }
 
 class _NewsPageState extends State<NewsPage> {
+  CategoryViewViewModel categoryViewViewMidel = CategoryViewViewModel();
+
+  @override
+  void initState() {
+    categoryViewViewMidel.fetchCategoryListApi();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Wether News"),
       ),
-      body: const Center(
-        child: Text(
-          "Wether News",
-          style: TextStyle(fontSize: 60),
-        ),
+      body: ChangeNotifierProvider<CategoryViewViewModel>(
+        create: (BuildContext context) => categoryViewViewMidel,
+        child: Consumer<CategoryViewViewModel>(builder: (context, value, _) {
+          switch (value.categoryList.status) {
+            case Status.LOADING:
+              return const CircularProgressIndicator();
+            case Status.ERROR:
+              return Text(value.categoryList.message.toString());
+            case Status.COMPLETED:
+              return ListView.builder(
+                  itemCount: value.categoryList.data!.products!.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: ListTile(
+                        leading: Image.network(value
+                            .categoryList.data!.products![index].thumbnail
+                            .toString()),
+                        title: Text(value
+                            .categoryList.data!.products![index].title
+                            .toString()),
+                        subtitle: Text(value
+                            .categoryList.data!.products![index].brand
+                            .toString()),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(value
+                                .categoryList.data!.products![index].rating
+                                .toString()),
+                            Text(value.categoryList.data!.products![index].price
+                                .toString()),
+                            const Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                            )
+                          ],
+                        ),
+                        //    onTap: Navigator.push(context, MaterialPageRoute(builder: (context) => CategoryDetailView)),
+                      ),
+                    );
+                  });
+          }
+          return Container();
+        }),
       ),
     );
   }
